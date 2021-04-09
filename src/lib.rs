78c20d138d7957b5bd8ef48f1118ce856b90aac2
袁殷 <2212079150@qq.com>
2021-04-09 11:23:38 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
+
+pub mod sink_config;
+
+#[cfg(test)]
+mod tests {
+    use crate::sink_config::{init_sink_config, get_sink_topic};
+    use std::collections::HashMap;
+
+    #[test]
+    fn it_works() {
+        let sin_conf_url = "http://web.rlink.17usoft.com/upgrade/config/name";
+        let application_name = "tlb_base_qa";
+        let timestamp = 123 as u64;
+
+        init_sink_config(sin_conf_url.to_string(), application_name.to_string());
+
+        let mut expression_param = HashMap::new();
+        expression_param.insert("timestamp".to_string(), timestamp.to_string());
+        let _sink_topic = get_sink_topic(expression_param);
+        println!()
+    }
+}
---
+++ src/sink_config.rs
@@ -0,0 +1,184 @@
+use std::collections::HashMap;
+use crate::sink_config::CompareType::{Greater, Smaller};
+use rlink::utils::http::client::get_sync;
+use std::error::Error;
+use dashmap::DashMap;
+
+lazy_static! {
+    static ref GLOBAL_SINK_CONFIG: DashMap<String, KafkaSinkContext> = DashMap::new();
+}
+
+pub const SINK_CONTEXT_KEY: &str = "sink_context";
+
+pub fn init_sink_config(sink_conf_url: String, application_name: String) {
+    let param = KafkaSinkConfParam::new(sink_conf_url, application_name);
+    tokio::runtime::Runtime::new()
+        .unwrap()
+        .block_on(load_remote_conf(&param));
+
+    std::thread::spawn(move || {
+        tokio::runtime::Runtime::new().unwrap().block_on(async {
+            loop {
+                tokio::time::delay_for(std::time::Duration::from_secs(60)).await;
+
+                load_remote_conf(&param).await;
+            }
+        });
+    });
+}
+
+pub fn get_sink_topic(expression_param: HashMap<String, String>) -> String {
+    let sink_config: &DashMap<String, KafkaSinkContext> = &*GLOBAL_SINK_CONFIG;
+    let sink_context = match sink_config.get(SINK_CONTEXT_KEY) {
+        Some(conf) => (*conf).clone(),
+        None => KafkaSinkContext::new(),
+    };
+    let mut expression = sink_context.expression.clone();
+    for (key, value) in expression_param {
+        expression = expression.replace(key.as_str(), value.as_str());
+    }
+    let result = match get_compare_type(expression.as_str()) {
+        Ok(compare_type) => match compare_type {
+            Greater(item1, item2) => item1 > item2,
+            Smaller(item1, item2) => item1 < item2,
+            _ => true,
+        },
+        Err(e) => {
+            error!("get compare type error!{}", e);
+            true
+        }
+    };
+    if result {
+        sink_context.topic_true.to_string()
+    } else {
+        sink_context.topic_false.to_string()
+    }
+}
+
+async fn load_remote_conf(param: &KafkaSinkConfParam) {
+    let sink_context =
+        get_config_data(param).unwrap_or(KafkaSinkContext::new());
+    let sink_config: &DashMap<String, KafkaSinkContext> = &*GLOBAL_SINK_CONFIG;
+    sink_config.insert(SINK_CONTEXT_KEY.to_string(), sink_context);
+}
+
+fn get_config_data(sink_conf_param: &KafkaSinkConfParam) -> Option<KafkaSinkContext> {
+    let url = format!(
+        "{}?applicationName={}",
+        sink_conf_param.url, sink_conf_param.application_name
+    );
+    match get_sync(url.as_str()) {
+        Ok(resp) => {
+            info!("sink config response={}", resp);
+            match parse_conf_response(resp.as_str()) {
+                Ok(response) => {
+                    let sink_context = response.result;
+                    Some(sink_context)
+                }
+                Err(e) => {
+                    error!("parse config response error. {}", e);
+                    None
+                }
+            }
+        }
+        Err(e) => {
+            error!("get sink config error. {}", e);
+            None
+        }
+    }
+}
+
+fn parse_conf_response(string: &str) -> serde_json::Result<ConfigResponse> {
+    let result: ConfigResponse = serde_json::from_str(string)?;
+    Ok(result)
+}
+
+enum CompareType {
+    Greater(i64, i64),
+    Smaller(i64, i64),
+    None,
+}
+
+fn get_compare_type(expression: &str) -> Result<CompareType, Box<dyn Error>> {
+    if expression.contains(">") {
+        let index = expression.find(">").unwrap();
+        Ok(Greater(
+            expression
+                .get(0..index)
+                .unwrap()
+                .trim()
+                .parse::<i64>()
+                .unwrap_or_default(),
+            expression
+                .get(index + 1..)
+                .unwrap()
+                .trim()
+                .parse::<i64>()
+                .unwrap_or_default(),
+        ))
+    } else if expression.contains("<") {
+        let index = expression.find("<").unwrap();
+        Ok(Smaller(
+            expression
+                .get(0..index)
+                .unwrap()
+                .trim()
+                .parse::<i64>()
+                .unwrap_or_default(),
+            expression
+                .get(index + 1..)
+                .unwrap()
+                .trim()
+                .parse::<i64>()
+                .unwrap_or_default(),
+        ))
+    } else {
+        Ok(CompareType::None)
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConfigResponse {
+    code: i32,
+    #[serde(default)]
+    message: String,
+    result: KafkaSinkContext,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct KafkaSinkContext {
+    #[serde(rename = "applicationName")]
+    application_name: String,
+    #[serde(rename = "expression")]
+    expression: String,
+    #[serde(rename = "topicTrue")]
+    topic_true: String,
+    #[serde(rename = "topicFalse")]
+    topic_false: String,
+}
+
+impl KafkaSinkContext {
+    pub fn new() -> Self {
+        KafkaSinkContext {
+            application_name: String::new(),
+            expression: String::new(),
+            topic_true: String::new(),
+            topic_false: String::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct KafkaSinkConfParam {
+    url: String,
+    application_name: String,
+}
+
+impl KafkaSinkConfParam {
+    pub fn new(url: String, application_name: String) -> Self {
+        KafkaSinkConfParam {
+            url,
+            application_name,
+        }
+    }
+}
