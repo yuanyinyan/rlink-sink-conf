ad085855c2d32b7acce79b26c5064d0dd2e47647
袁殷 <2212079150@qq.com>
2021-04-13 10:21:11 +0800

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! # rlink-sink-conf
+//!
+//! A library to upgrade [rlink](https://docs.rs/rlink) task smoothly.
+//!
+//! # Example
+//!
+//! ```
+//! use rlink_sink_conf::sink_config::{init_sink_config, get_sink_topic};
+//! use std::collections::HashMap;
+//!
+//! let sin_conf_url = "http://web.rlink.17usoft.com/upgrade/config/name";
+//! let application_name = "tlb_base_qa";
+//! let timestamp = 123 as u64;
+//!
+//! init_sink_config(sin_conf_url.to_string(), application_name.to_string());
+//!
+//! let mut expression_param = HashMap::new();
+//! expression_param.insert("timestamp".to_string(), timestamp.to_string());
+//! let sink_topic = get_sink_topic(expression_param);
+//! ```
+
 #[macro_use]
 extern crate log;
 #[macro_use]
